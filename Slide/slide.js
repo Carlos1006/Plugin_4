c196c80865357e66400b1bcaebd7c62b7b67e576
Naker21 <cdgzz19@gmail.com>
2015-10-22 22:43:47 -0500

    Creacion automatica del slide
    El slide se crea automaticamente en el div seleccionado

--- Slide/slide.js
+++ Slide/slide.js
@@ -1,3 +1,4 @@
+var div="div",span = "span",ul="ul",a="a",li="li",lbl="label",img="img",p="p",swing="swing",linea="linear";
 ;(function($,Math,Document,Window,undefined) {
     $.attrHooks['attributename'] = {
         set: function(elem, value, name) {
@@ -75,7 +76,12 @@
     function getPath(name) {
         return "http://localhost/__naker/Plugin_4/Slide/rsc/"+name;  
     };
-    var div = "div";
+    function tagForm(name) {
+        return "<"+name+">";
+    };
+    function getClass(name) {
+        return {class:name};  
+    };
     function PDF_Slider(container) {
         this.init(container);
         this.define();
@@ -88,6 +94,7 @@
         this.globalId   = makeid();
         this.main.addClass("superSlide");
         this.main.attr("globalId",this.globalId);
+        this.createForm();
     };
     PDF_Slider.prototype.define     = function() {
         this.header     = this.main.find(".slideHeader");
@@ -150,6 +157,9 @@
         });
         this.main.mouseleave(function(){
             _this.mov.addClass("fadeOut");
+            clearInterval(_this.interval);
+        });
+        $(document).mouseup(function(){
             clearInterval(_this.interval);
         });
         this.mov
@@ -256,21 +266,50 @@
             imargLeft(img);
         }
     };
-    PDF_Slider.prototype.createTile = function(name,img) {
-        var $dlc = $("<div>",{class:"dlc"});
-        var $div = $("<div>");
-        var $img = $("<img>",{src:img});
-        var $span = $("<span>",{text:name});
-        var $dwld = $("<div>",{class:"download"});
-        var $link = $("<a>",{name:name,href:getPath(name)});
-        var $svg = "<svg viewBox='109 -109.5 268.5 268.5' fill='rgb(200,200,200)'><path d='M341.9,114.3H144.5c-12.2,0-22.1,10-22.1,22.4c0,12.4,9.9,22.4,22.1,22.4h197.4c12.2,0,22.1-10,22.1-22.4C364.1,124.3,354.2,114.3,341.9,114.3z M226.9,89.7c4,4,9.2,6.1,14.5,6.5c0.6,0.1,1.2,0.2,1.9,0.2c0.6,0,1.2-0.1,1.8-0.2c5.3-0.4,10.5-2.5,14.5-6.5l70.8-71c8.9-8.9,8.9-23.3,0-32.1c-8.9-8.9-23.2-8.9-32.1,0l-32.7,32.8V-87.1c0-12.4-10-22.4-22.4-22.4c-12.4,0-22.4,10-22.4,22.4V19.3l-32.7-32.8c-8.9-8.9-23.2-8.9-32.1,0c-8.9,8.9-8.9,23.3,0,32.1L226.9,89.7z'/></svg>";
+    PDF_Slider.prototype.createTile   = function(name,img_) {
+        var $dlc    = this.createObj(div,getClass("dlc"));
+        var $div    = this.createObj(div);
+        var $img    = this.createObj(img,{src:img_});
+        var $span   = this.createObj(span,{text:name});
+        var $dwld   = this.createObj(div,getClass("download"));
+        var $link   = this.createObj(a,{name:name,href:getPath(name)});
+        var $svg    = "<svg viewBox='109 -109.5 268.5 268.5' fill='rgb(200,200,200)'><path d='M341.9,114.3H144.5c-12.2,0-22.1,10-22.1,22.4c0,12.4,9.9,22.4,22.1,22.4h197.4c12.2,0,22.1-10,22.1-22.4C364.1,124.3,354.2,114.3,341.9,114.3z M226.9,89.7c4,4,9.2,6.1,14.5,6.5c0.6,0.1,1.2,0.2,1.9,0.2c0.6,0,1.2-0.1,1.8-0.2c5.3-0.4,10.5-2.5,14.5-6.5l70.8-71c8.9-8.9,8.9-23.3,0-32.1c-8.9-8.9-23.2-8.9-32.1,0l-32.7,32.8V-87.1c0-12.4-10-22.4-22.4-22.4c-12.4,0-22.4,10-22.4,22.4V19.3l-32.7-32.8c-8.9-8.9-23.2-8.9-32.1,0c-8.9,8.9-8.9,23.3,0,32.1L226.9,89.7z'/></svg>";
         $dwld.append($svg);
         $img.on("load",this.adjustTile_2);
         $div.append($img);
         $dlc.append($div,$span,$dwld,$link);
-        this.body.find(".dlcContainer").prepend($dlc);
+        this.body.find(".dlcContainer").append($dlc);
         this.tileEvents(".dlc");
         jmargTop.call(this.body,$dlc);
+        if(this.topSave == undefined) {
+            this.topSave = this.body.find(".dlc").attr("style");
+        }
+    };
+    PDF_Slider.prototype.createSvg    = function(points) {
+        return "<svg viewBox='0 0 50 50' preserveAspectRatio='none'><polygon fill='rgb(200,200,200)' points='"+points.join(" ")+"'></polygon></svg>";
+    };
+    PDF_Slider.prototype.createObj    = function(tag,attr) {
+        return $(tagForm(tag),attr);
+    };
+    PDF_Slider.prototype.createForm   = function() {
+        this.main.empty();
+        var h       = this.createObj(div,getClass("slideHeader"));
+        var h_span  = this.createObj(span,{text:"Contenido Descargable"});
+        var h_div   = this.createObj(div);
+        var h_div_svg   = "<svg viewBox='0 0 50 50' preserveAspectRatio='none'><polygon fill='rgb(200,200,200)' points='00,00 26,00 26,10 00,10'><animate begin='indefinite'  class='toBar' to='00,00 20,00 20,11 00,11'></animate><animate begin='indefinite'  class='toSqr' to='00,00 26,00 26,10 00,10'></animate></polygon><polygon fill='rgb(200,200,200)' points='00,20 26,20 26,25 00,25'><animate begin='indefinite'  class='toBar' to='00,10 20,10 20,20 00,20'></animate><animate begin='indefinite'  class='toSqr' to='00,20 26,20 26,25 00,25'></animate></polygon><polygon fill='rgb(200,200,200)' points='00,24 26,24 26,30 00,30'><animate begin='indefinite'  class='toBar' to='00,30 20,30 20,41 00,41'></animate><animate begin='indefinite'  class='toSqr' to='00,24 26,24 26,30 00,30'></animate></polygon><polygon fill='rgb(200,200,200)' points='00,40 26,40 26,50 00,50'><animate begin='indefinite'  class='toBar' to='00,40 20,40 20,50 00,50'></animate><animate begin='indefinite'  class='toSqr' to='00,40 26,40 26,50 00,50'></animate></polygon><polygon fill='rgb(200,200,200)' points='24,00 50,00 50,10 24,10'><animate begin='indefinite'  class='toBar' to='30,00 50,00 50,11 30,11'></animate><animate begin='indefinite'  class='toSqr' to='24,00 50,00 50,10 24,10'></animate></polygon><polygon fill='rgb(200,200,200)' points='24,20 50,20 50,25 24,25'><animate begin='indefinite'  class='toBar' to='30,10 50,10 50,20 30,20'></animate><animate begin='indefinite'  class='toSqr' to='24,20 50,20 50,25 24,25'></animate></polygon><polygon fill='rgb(200,200,200)' points='24,24 50,24 50,30 24,30'><animate begin='indefinite'  class='toBar' to='30,30 50,30 50,41 30,41'></animate><animate begin='indefinite'  class='toSqr' to='24,24 50,24 50,30 24,30'></animate></polygon><polygon fill='rgb(200,200,200)' points='24,40 50,40 50,50 24,50'><animate begin='indefinite'  class='toBar' to='30,40 50,40 50,50 30,50'></animate><animate begin='indefinite'  class='toSqr' to='24,40 50,40 50,50 24,50'></animate></polygon></svg>";
+        var b           = this.createObj(div,getClass("slideBody"));
+        var b_dlcC      = this.createObj(div,getClass("dlcContainer"));
+        var l           = this.createObj(div,getClass("rightMove fadeOut"));
+        var r           = this.createObj(div,getClass("leftMove fadeOut"));
+        var r_svg       = "<svg viewBox='201 -200.8 451.8 451.8' fill='rgb(200,200,200)'><path d='M298.1,25.1c0-8.1,3.1-16.2,9.3-22.4l194.3-194.3c12.4-12.4,32.4-12.4,44.8,0c12.4,12.4,12.4,32.4,0,44.7L374.5,25.1 L546.4,197c12.4,12.4,12.4,32.4,0,44.7c-12.4,12.4-32.4,12.4-44.7,0L307.4,47.4C301.2,41.3,298.1,33.2,298.1,25.1z'/></svg>";
+        var l_svg       = "<svg viewBox='201 -200.8 451.8 451.8' fill='rgb(200,200,200)'><path d='M546.4,47.4L352.2,241.7c-12.4,12.4-32.4,12.4-44.8,0c-12.4-12.4-12.4-32.4,0-44.7L479.3,25.1L307.4-146.8 c-12.4-12.4-12.4-32.4,0-44.7c12.4-12.4,32.4-12.4,44.8,0L546.4,2.7c6.2,6.2,9.3,14.3,9.3,22.4C555.7,33.2,552.6,41.3,546.4,47.4z'/></svg>";
+        h_div.append(h_div_svg);
+        h.append(h_span,h_div);
+        b.append(b_dlcC);
+        l.append(l_svg);
+        r.append(r_svg);
+        this.main.append(h,b,l,r);
+        this.main.trigger("mainFormReady");
     };
     
     $.fn.PDF_Slider = function() {
