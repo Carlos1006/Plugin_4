5f40155f77fc65585f8d8f2a404954df79c68bee
Naker21 <cdgzz19@gmail.com>
2015-11-18 01:32:32 -0600

    Correccion en Eventos y Footer

--- Noticias/Script/Eventos.js
+++ Noticias/Script/Eventos.js
@@ -10,12 +10,14 @@ $(function(){
         };
         var request = $.ajax(params);
         request.success(function(responde) {
+            var toggle = true;
             for(var i in responde) {
+                toggle = !toggle;
                 var obj = responde[i];
                 var event = $.getEvent(obj.h,obj.name,obj.img);
                 $(".tabContainer").setEvent(obj.y,obj.m-1,obj.d,event);
                 var dateA = $.getDate(obj.d,obj.m-1,obj.y);
-                var color = $.getColor(32,66,136);
+                var color = toggle?$.getColor(46,87,171):$.getColor(238,27,46);
                 $(".tabContainer").setRange(dateA,dateA,color);
                 $(".tabContainer").refresh();
             }
